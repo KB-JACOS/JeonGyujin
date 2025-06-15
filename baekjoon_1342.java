df867720dea94b552f4048d8bf99f99a1b13cd1e
jeonkyujin <954qew@naver.com>
2025-06-15 16:48:11 +0900

    refactor: baekjoon_1342 - 행운의 문자열

--- baekjoon_1342.java
+++ baekjoon_1342.java
@@ -1,73 +1,45 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class baekjoon_1342 {
 
+	static int[] alpha = new int[26];
 
+	static int N;
 
-	static boolean [] visited;
+	static int result = 0;
 
-	static Set<String> store = new HashSet<>();
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String input = br.readLine();
+		N = input.length();
 
-		if (check2(input)){
-			int answer1 = 1;
-			for (int i = input.length(); i >= 2 ; i--) {
-				answer1 *= i;
-			}
-			System.out.println(answer1);
+		for (char c : input.toCharArray()) {
+			alpha[c - 'a'] += 1;
+		}
+		dfs(0, ' ');
+		System.out.println(result);
+
+	}
+
+	public static void dfs(int depth, char prev) {
+		if (depth == N) {
+			result += 1;
 			return;
 		}
-		visited = new boolean[input.length()];
-		dfs(0, input, "");
-		int answer = 0;
-		for(String value : store){
-			if (check(value)){
-				answer +=1;
-			}
-		}
-		System.out.println(answer);
+		for (int i = 0; i < 26; i++) {
+			if (alpha[i] == 0)
+				continue;
+			char curr = (char)('a' + i);
+			if (prev == curr)
+				continue;
 
-	}
-
-	public static void dfs(int depth, String input, String current){
-		if (depth == input.length()){
-			store.add(current);
-			return;
+			alpha[i]--;
+			dfs(depth + 1, curr);
+			alpha[i]++;
 		}
-		for (int i = 0; i < input.length(); i++) {
-			if(!visited[i]){
-				visited[i] = true;
-				dfs(depth+1, input, current + input.charAt(i));
-				visited[i] = false;
-			}
-		}
-	}
-
-	public static boolean check2(String str){
-		List<String> a = new ArrayList<>();
-		a.add(String.valueOf(str.charAt(0)));
-		for (int i = 1; i < str.length(); i++) {
-			if (a.contains(String.valueOf(str.charAt(i)))){
-				return false;
-			};
-			a.add(String.valueOf(str.charAt(i)));
-		}
-		return true;
-	}
-
-	public static boolean check(String str) {
-		for (int i = 1; i < str.length(); i++) {
-			if (str.charAt(i) == str.charAt(i-1)){
-				return false;
-			}
-		}
-		return true;
 	}
 }
+
+
+
