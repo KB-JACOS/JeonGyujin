e11dc77b42fbc7d6f222b306c70afb5c99d0b039
jeonkyujin <954qew@naver.com>
2025-06-20 17:50:27 +0900

    refactor: baekjoon_1802 - 종이 접기

--- baekjoon_1802.java
+++ baekjoon_1802.java
@@ -1,67 +1,49 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
 
 public class baekjoon_1802 {
-	static List<String> answer = new ArrayList<>();
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int num = Integer.parseInt(br.readLine());
-		for (int i = 0; i < num; i++) {
-			String str = br.readLine();
-			answer.add(check(str));
-		}
-		for (int i = 0; i < answer.size(); i++) {
-			bw.write(answer.get(i));
-			bw.newLine();
-		}
-
-		bw.flush();
-		bw.close();
-	}
-
-	public static String check(String a) {
-		Queue<String> store = new ArrayDeque<>();
-		store.offer(a);
-		boolean checkValue = true;
-		while (!store.isEmpty()) {
-			String aa = store.poll();
-			int mid = aa.length() / 2;
-			if (aa.length() == 1) {
-				break;
-			}
-			String str1 = aa.substring(0, mid);
-			String str2 = aa.substring(mid + 1);
-			if (!compare(str1, str2)) {
-				checkValue = false;
-				break;
-
-			}
-			store.offer(str1);
-			store.offer(str2);
-
-		}
-		return checkValue ? "YES" : "NO";
-		
-	}
-
-	public static boolean compare(String s1, String s2) {
-		boolean checkValue = true;
-		for (int i = 0; i < s1.length(); i++) {
-			if (String.valueOf(s1.charAt(i)).equals(String.valueOf(s2.charAt(s1.length() - i - 1)))) {
-				checkValue = false;
-				break;
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			String s = br.readLine();
+			if (solve(s)) {
+				System.out.println("YES");
+			} else {
+				System.out.println("NO");
 			}
 		}
-		return checkValue;
+	}
+	public static boolean solve(String s){
+		if (s.length() == 1){
+			return true;
+		}
+
+		int idx = s.length() / 2 ;
+		String str1 = s.substring(0, idx);
+		String str2 = s.substring(idx+1);
+
+		if (check(str1, str2)){
+			//재귀 계속 돌리기
+			return solve(str1) & solve(str2);
+
+		}else{
+			return false;
+		}
+
+
 	}
 
+	public static boolean check(String str1, String str2){
+		StringBuilder sb = new StringBuilder(str2);
+		String a = sb.reverse().toString();
+
+		for (int i = 0; i < str1.length(); i++) {
+			if (str1.charAt(i) == a.charAt(i)){
+				return false;
+			}
+		}
+		return true;
+	}
 }
